9f46a15cdca12f0d143c2e5ecbd5d28ed4dd037c
priyankamendpara <priya.soft.test@gmail.com>
2020-06-06 23:32:56 +0530

    javafirsthomework

---
+++ IdeaProjects/Javafirst Homework/src/MyDetails.java
@@ -0,0 +1,39 @@
+public class MyDetails {
+
+    public static void main(String[] area) {
+
+        String name = " Name = priya";
+        System.out.println(name);
+
+        String address =  " Address = 187 torbay road nottingham ng2 4qe " ;
+        System.out.println(address);
+
+        String detail = "Age = ";
+        int age = 25;
+        System.out.print(detail);
+        System.out.println(age);
+
+        String number = "Mobile number = ";
+        String mobilenumber = "8866446188";
+        System.out.print(number);
+        System.out.println(mobilenumber);
+
+        String income = "Salary = ";
+        int salary =  10000;
+        System.out.print(income);
+        System.out.println(salary);
+
+        String mail = "Gmail ID = ";
+        String gmail ="priyahirapara211@gmail.com";
+        System.out.print(mail);
+        System.out.println(gmail);
+
+        String same = "priya ",urname= "hirapara",ddress="abc";
+        System.out.println(same+urname);
+        System.out.println(ddress);
+
+
+
+
+    }
+}
